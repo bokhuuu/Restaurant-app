c26da023a1a892bb6d796be162058aa35da0173c
bokhuuu <zurabokhua@gmail.com>
2023-08-10 16:20:54 +0400

    implemented AboutUs component

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
-
+import { images } from "../../constants"
 import './AboutUs.css';
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div className='aboutus bg flex-center section-padding' id="about">
+
+    <div className='aboutus-overlay flex-center'>
+      <img src={images.G} alt='G-overlay' />
+    </div>
+
+    <div className='aboutus-content flex-center'>
+      <div className='aboutus-content-about'>
+        <h1 className='headtext-cormorant'>About Us</h1>
+        <img src={images.spoon} alt='spoon' className='spoon-img' />
+        <p className='p-opensans'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
+        <button type='button' className='custom-button'>Know More</button>
+      </div>
+
+      <div className='aboutus-content-knife flex-center'>
+        <img src={images.knife} alt='knife' />
+      </div>
+
+      <div className='aboutus-content-history'>
+        <h1 className='headtext-cormorant'>Our History</h1>
+        <img src={images.spoon} alt='spoon' className='spoon-img' />
+        <p className='p-opensans'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
+        <button type='button' className='custom-button'>Know More</button>
+      </div>
+    </div>
+
   </div>
 );
 
