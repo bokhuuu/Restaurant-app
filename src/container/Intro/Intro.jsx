edb9edc12ace3ae165cb2bd8195347a0cdf58b14
bokhuuu <zurabokhua@gmail.com>
2023-08-11 17:08:33 +0400

    implemented Intro component where is used video for introduction

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
-
+import React, { useState, useRef } from 'react';
+import { BsFillPlayFill, BsFillPauseFill } from "react-icons/bs";
+import { meal } from '../../constants';
 import './Intro.css';
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+  const [playVideo, setPlayVideo] = useState(false)
+  const vidRef = useRef()
+
+  const handleVideo = () => {
+    setPlayVideo((prevplayVideo) => !prevplayVideo)
+
+    if (playVideo) {
+      vidRef.current.pause()
+    } else {
+      vidRef.current.play()
+    }
+  }
+
+  return (
+    <div className='video'>
+      <video
+        src={meal}
+        ref={vidRef}
+        controls={false}
+        muted
+        typeof='video/mp4'
+        loop
+      />
+
+      <div className='video-overlay flex-center'>
+        <div
+          className='video-overlay-circle flex-center'
+          onClick={handleVideo}
+        >
+
+          {
+            playVideo ? <BsFillPauseFill color="#fff" fontSize={30} />
+              : <BsFillPlayFill color="#fff" fontSize={30} />
+          }
+
+        </div>
+      </div>
+    </div>
+  )
+};
 
 export default Intro;
