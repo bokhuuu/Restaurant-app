e37bb2fc727f91a3e5aa5bc0875a773d2c866ee1
bokhuuu <zurabokhua@gmail.com>
2023-08-13 12:52:40 +0400

    impplemented Laurels component,whitch uses AwardCard component to..

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
+import { images, data } from '../../constants';
 import './Laurels.css';
 
+const AwardCard = ({ award: { imgUrl, title, subtitle } }) => (
+    <div className="laurels-awards-card">
+        <img src={imgUrl} alt="awards" />
+        <div className="laurels-awards-card-content">
+            <p className="p-cormorant" style={{ color: '#DCCA87' }}>{title}</p>
+            <p className="p-opensans">{subtitle}</p>
+        </div>
+    </div>
+);
+
 const Laurels = () => (
-  <div>
-    Laurels
-  </div>
+    <div className="bg wrapper section-padding" id="awards">
+        <div className="wrapper_info">
+            <SubHeading title="Awards & recognition" />
+            <h1 className="headtext-cormorant">Our Laurels</h1>
+
+            <div className="laurels-awards">
+                {data.awards.map((award) => <AwardCard award={award} key={award.title} />)}
+            </div>
+        </div>
+
+        <div className="wrapper-img">
+            <img src={images.laurels} alt="laurels" />
+        </div>
+    </div>
 );
 
 export default Laurels;
